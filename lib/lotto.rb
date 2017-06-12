dbc91e016664b58dd74296b19dc859441381b911
Alex Filatau <filatau@gmail.com>
2017-06-12 10:35:50 -0700

    Extract lotto data fetch and match into separate lib

---
+++ lib/lotto.rb
@@ -0,0 +1,49 @@
+require 'restclient'
+require 'nokogiri'
+
+module  Lotto
+
+  def fetch_mega_millions
+    link = "http://www.calottery.com/play/draw-games/mega-millions/Winning-Numbers/"
+    page = Nokogiri::HTML(RestClient.get(link))
+    draws = {}
+
+    data = page.css("tr").map {|x| x.css("td")  };
+    data = data.select {|x| !x.empty?}
+
+    data.each do |row|
+      date, draw = row[0].css("span").text.split(" - ")
+      numbers = row[1].css("span").to_a
+        .reduce([]){|s, num| s << num.text  }
+        .reject {|x| x.empty?}
+        .map{|x| x.to_i}
+      mega = row[2].text.to_i
+
+      draws[draw.to_i] = {
+        date: date,
+        numbers: numbers,
+        mega: mega
+      }
+    end
+
+    return draws
+  end
+
+  def find_matches(draw_results, ticket)
+    matches_found = []
+    draw_results.each do |draw_number, value|
+      numbers_to_check = value[:numbers] + [value[:mega]]
+      common = ( numbers_to_check & ticket)
+      if !common.empty?
+        matches_found << {
+          draw_number: draw_number,
+          date: value[:date],
+          matching_numbers: common
+        }
+      end
+    end
+    return matches_found
+  end
+
+
+end
--- mega.rb
+++ mega.rb
@@ -1,44 +1,23 @@
-require 'restclient'
-require 'nokogiri'
+require 'pp'
 require_relative 'lib/colorize'
+require_relative 'lib/lotto'
 
 include Colorize
-
-link = "http://www.calottery.com/play/draw-games/mega-millions/Winning-Numbers/"
+include Lotto
 
 b_numbers = [1, 2, 3, 4, 5, 6]
 
-page = Nokogiri::HTML(RestClient.get(link))
-draws = {}
-
-data = page.css("tr").map {|x| x.css("td")  };
-data = data.select {|x| !x.empty?}
-
-data.each do |row|
-  date, draw = row[0].css("span").text.split(" - ")
-  numbers = row[1].css("span").to_a
-    .reduce([]){|s, num| s << num.text  }
-    .reject {|x| x.empty?}
-    .map{|x| x.to_i}
-  mega = row[2].text.to_i
-
-  draws[draw.to_i] = {
-    date: date,
-    numbers: numbers,
-    mega: mega
-  }
-end
+draws = fetch_mega_millions()
 
 puts "Last 20 draws:"
 pp draws
 puts
 
 puts "Betting on: #{green(b_numbers.join(' '))}"
-draws.each do |draw_number, value|
-  numbers_to_check = value[:numbers] + [value[:mega]]
-  common = ( numbers_to_check & b_numbers)
-  if !common.empty?
-    puts "Winning draw #{yellow(draw_number)} on #{yellow(value[:date])}:
-          matching #{red(common.join(' '))}"
-  end
+
+winning_numbers = find_matches(draws, b_numbers)
+
+winning_numbers.each do |wn|
+  puts "Winning draw #{yellow(wn[:draw_number])} on #{yellow(wn[:date])}:
+        matching #{red(wn[:matching_numbers].join(' '))}"
 end
